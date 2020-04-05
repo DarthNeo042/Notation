4bc784b1ea4c05be7cc242497c9c9b0682c6d4e2
benoit.piard42@gmail.com <benoit.piard42@gmail.com>
2020-04-05 13:55:41 +0100

    Début RDLC période

--- Notation/Reports/BulletinPeriodeHeaderDataSource.cs
+++ Notation/Reports/BulletinPeriodeHeaderDataSource.cs
@@ -8,5 +8,8 @@
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string BirthDate { get; set; }
+        public string Column1Libelle { get; set; }
+        public string Column2Libelle { get; set; }
+        public string Column3Libelle { get; set; }
     }
 }
---
+++ Notation/Reports/BulletinPeriodeLineDataSource.cs
@@ -0,0 +1,13 @@
+﻿namespace Notation.Reports
+{
+    public class BulletinPeriodeLineDataSource
+    {
+        public bool IsChildSubject { get; set; }
+        public string Subject { get; set; }
+        public string Coefficient { get; set; }
+        public string Teacher { get; set; }
+        public string Marks1 { get; set; }
+        public string Marks2 { get; set; }
+        public string Marks3 { get; set; }
+    }
+}
