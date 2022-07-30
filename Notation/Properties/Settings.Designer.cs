73923a9bf11df13ad163012e53a1063730a66ad0
Neo Darth <DarthNeo042@protonmail.com>
2022-07-30 11:39:06 +0100

    Modification

--- Notation/Properties/Settings.Designer.cs
+++ Notation/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Notation.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.0.3.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -89,8 +89,8 @@ namespace Notation.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=localhost\\SQLEXPRESS;Initial Catalog=NotationCollege;User Id=admin;Pa" +
-            "ssword=admin")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=MSI\\SQLEXPRESS;Initial Catalog=NotationCollege;Integrated Security=Tr" +
+            "ue")]
         public string SQLConnection {
             get {
                 return ((string)(this["SQLConnection"]));
